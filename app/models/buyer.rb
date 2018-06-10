e40f76fcf5e714fbfc8a701b566c9db76854480e
Josh Young <j22cal@gmail.com>
2018-06-10 17:05:09 -0500

    Setup model relationships

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,5 @@
 class Buyer < ApplicationRecord
+  #The 'dependent: :destroy' says to destroy the satisfaction
+  #ranking for that buyer when the buyer is removed:
+  has_many :satisfactions, dependent: :destroy
 end
--- app/models/satisfaction.rb
+++ app/models/satisfaction.rb
@@ -1,2 +1,4 @@
 class Satisfaction < ApplicationRecord
+  belongs_to :product
+  belongs_to :buyer
 end
